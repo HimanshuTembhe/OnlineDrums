fae38b179fd5b561f14bf9d407a84124fd84d378
Himanshu333 <99601153+HimanshuTembhe@users.noreply.github.com>
2024-03-12 10:24:43 +0530

    git init

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,34 @@
+
+for(let i = 0; i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        //document.querySelectorAll(".drum")[i].style.color="black";
+        //this.style.color="black";
+
+        var btnValue = this.textContent;
+        keyPress(btnValue);
+        
+
+         
+    });
+}
+
+function keyPress(btnValue){
+    switch(btnValue){
+        case "w":
+            var drumSound = new Audio("./sounds/tom-1.mp3");
+            drumSound.play();
+            break;
+
+        case "a":
+            var drumSound = new Audio("./sounds/tom-2.mp3");
+            drumSound.play();
+            break;
+
+    }
+}
+
+document.addEventListener("keydown",function(event){
+    //console.log(event);
+    keyPress(event.key);
+})
+
